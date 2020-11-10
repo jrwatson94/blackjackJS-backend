025f490d559e666780866cda9d469dee6f4cddb4
nah1222 <nah1222@yahoo.com>
2020-11-10 11:34:02 -0500

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
+    
     def show 
         @user = User.find(params[:id])
         render json: @user
         
     end
+    def new
+        @user = User.new
+        render json: @user
+    end
+    def create
+        @user = User.create(user_params)
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :money, :wins, :losses)
+    end
+
 end
